3ace2065faa94c67fbb78eefd3f40f39ae6da37b
Jordan Bramble <jbramble@gmu.edu>
2013-09-17 02:19:29 -0400

    Updated Carto

--- Project-Laphroaig18/CartoTweets.js
+++ Project-Laphroaig18/CartoTweets.js
@@ -9,29 +9,41 @@ var twit = new twitter({consumer_key: '',
   access_token_key: '',
   access_token_secret: ''});
 
-
-client.on('connect', function() {
     console.log("connected");
 
     // template can be used
     client.query("SELECT * FROM places", function(err, data){
-    	var rows = data.rows;
-    	//console.log(rows);
-    	for (var i = 0; i<rows.length; i++){
-    		//console.log(rows[i]);
-    		twit.get('search/tweets',{q: rows[i].placename}, function(err, item){
-    			var tweets = item.statuses;
-    			for (var j = 0; j<tweets.length; j++){
-    				console.log(tweets[j].text);
-                    //this should print allthe tweets returned. however the query below causes an error related to item.statuses?? I have to do HW and cant fix ATM. :(
-    				//client.query("INSERT INTO posts (the_geom, lat, lon, placename, state, post_text, tweetid) VALUES(null, {lat}, {lon}, {placename}, {state}, {text}, {tweetid})", {lat: rows[i].lat, lon: rows[i].lon, placename: rows[i].placename, state: rows[i].state, text: tweets[j].text, tweetid: tweets[j].id}, function(err,data){})
-    			}
+        var rows = data.rows;
+        var  tweets;
+        //console.log(rows);
+        for (var i = 0; i<rows.length; i++){
+            //console.log(rows[i]);
+            var currRow = rows[i];
+            twit.get('search/tweets',{q: currRow.placename}, function(err, item){
+                //var tweets = item.statuses;
+                var tweets = item.statuses;
 
-    		})
-    	}
+                for (var j = 0; j<tweets.length; j++){
+                    //console.log(tweets[j].text);
+                    //console.log('lon'+rows[i].lon);
+                    var currentTweet=tweets[j];
+                    var TweetText = currentTweet.text;
+                    console.log(currRow.placename);
+
+                    client.query("INSERT INTO posts (the_geom, lat, lon, placename, state, post_text, tweetid) VALUES(null, {lat}, {lon}, {placename}, {state}, {text}, {tweetid})", {lat: currRow.lat, lon: currRow.lon, placename: currRow.placename, state: currRow.state, text: currentTweet.text, tweetid: currentTweet.id}, function(err,data){
+                        console.log(currentTweet.text+ '|'+ TweetText + '|'+ err);
+                    })
+
+                }
+
+            })
+
+
+                
+        }
     })
 
-    	
+        
 
 
     // JSON parsed data or error messages are returned
