fbafa306a8daeb4d8e50170bc5118075175c992b
Josh Essex <josh.essex@opower.com>
2013-09-17 23:21:08 -0700

    Cleans up the formatting a bit and introduces command line argument parsing.

--- Project-Laphroaig18/CartoTweets.js
+++ Project-Laphroaig18/CartoTweets.js
@@ -1,13 +1,13 @@
 var twitter = require('mtwitter');
 var CartoDB = require('cartodb');
+var argv = require('optimist').argv;
 
+var client = new CartoDB({user: argv.user, api_key: argv.api_key});
 
-var client = new CartoDB({user: '',api_key: ''});
-
-var twit = new twitter({consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''});
+var twit = new twitter({consumer_key: argv.consumer_key,
+                        consumer_secret: argv.consumer_secret,
+                        access_token_key: argv.access_token_key,
+                        access_token_secret: argv.access_token_secret});
 
 client.on('connect', function() {
     console.log("connected");
@@ -15,37 +15,35 @@ client.on('connect', function() {
     // template can be used
     client.query("SELECT * FROM places", function(err, data){
         var rows = data.rows;
-        var  tweets;
-        //console.log(rows);
-        for (var i = 0; i<rows.length; i++){
-            //console.log(rows[i]);
+        var tweets;
+        
+        for (var i = 0; i < rows.length; i++){
             var currRow = rows[i];
-            twit.get('search/tweets',{q: currRow.placename}, function(err, item){
-                //var tweets = item.statuses;
+
+            twit.get('search/tweets', {q: currRow.placename}, function(err, item){                
                 var tweets = item.statuses;
 
-                for (var j = 0; j<tweets.length; j++){
-                    //console.log(tweets[j].text);
-                    //console.log('lon'+rows[i].lon);
-                    var currentTweet=tweets[j];
-                    var TweetText = currentTweet.text;
+                for (var j = 0; j < tweets.length; j++){
+                    var currentTweet = tweets[j];
+                    var tweetText = currentTweet.text;
+                    
                     console.log(currRow.placename);
-                    var sqlCommand = "INSERT INTO posts (the_geom, lat, lon, placename, state, post_text, tweetid) VALUES(null, {lat}, {lon}, {placename}, {state}, {text}, {tweetid})";
-                    var jsonvars = {lat: currRow.lat, lon: currRow.lon, placename: currRow.placename, state: currRow.state, text: currentTweet.text, tweetid: currentTweet.id};
-                    client.query(sqlCommand, jsonvars , function(err,data){
-                        console.log(currentTweet.text+ '|'+ TweetText + '|'+ err);
-                    })
+                    
+                    var sql = "INSERT INTO posts (the_geom, lat, lon, placename, state, post_text, tweetid) VALUES(null, {lat}, {lon}, {placename}, {state}, {text}, {tweetid})";
+                    var json = {lat: currRow.lat, 
+                                lon: currRow.lon, 
+                                placename: currRow.placename, 
+                                state: currRow.state, 
+                                text: tweetText, 
+                                tweetid: currentTweet.id};
+                    
+                    client.query(sql, json , function(err, data){
+                        console.log(tweetText + '|'+ err);
+                    });
                 }
-
-            })
-
-
-                
+            }); 
         }
-    })
-
-        
-
+    });
 
     // JSON parsed data or error messages are returned
 
